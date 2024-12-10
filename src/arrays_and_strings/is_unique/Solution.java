b8afe558026bd3afb6e914405cb0573da0ba3ef3
Vuk Perovic <vukperovic03@gmail.com>
2024-12-10 15:08:21 +0100

    Changed project structure

---
+++ src/arrays_and_strings/is_unique/Solution.java
@@ -0,0 +1,4 @@
+package arrays_and_strings.is_unique;
+
+public class Solution {
+}
---
+++ src/arrays_and_strings/is_unique/Test.java
@@ -0,0 +1,7 @@
+package arrays_and_strings.is_unique;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
